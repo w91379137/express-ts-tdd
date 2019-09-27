4958ead1d69bc6ffe7538db11698a828398bb80b
w91379137 <w913791379137@gmail.com>
2019-09-28 05:23:48 +0800

    新增 source-map-support

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,19 @@
 
+declare function require(name: string);
+require('source-map-support').install();
+
 console.log('Build env:', env);
+
+class Foo {
+    constructor() { this.bar(); }
+    bar() { throw new Error('this is a demo'); }
+}
+new Foo();
+
+// 這樣不行
+// throw new Error('Test');
+
+// 這個可以
+// setTimeout(() => {
+//     throw new Error('Test');
+// }, 1000);
