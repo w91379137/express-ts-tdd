f2a54ee36860eca8fb0e23ca9078ec6d438b7445
w91379137 <w913791379137@gmail.com>
2019-09-30 03:50:07 +0800

    設定 debug print 功能

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 
-declare function require(name: string);
 require('source-map-support').install();
 
-console.log('Build env:', env);
+let debug_AC = require('debug')('app:a:c')
+let debug_BC = require('debug')('app:b:c')
+let debug_AB = require('debug')('app:a:b')
 
-class Foo {
-    constructor() { this.bar(); }
-    bar() { throw new Error('this is a demo'); }
-}
-new Foo();
+setTimeout(() => {
+    debug_AC('Build env_ac:' + env);
+    debug_BC('Build env_bc:' + env);
+    debug_AB('Build env_ab:' + env);
+}, 100);
+
+// class Foo {
+//     constructor() { this.bar(); }
+//     bar() { throw new Error('this is a demo'); }
+// }
+// new Foo();
 
 // 這樣不行
 // throw new Error('Test');
