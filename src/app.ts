fc0edc0067bfe57b10ee2f95750653e3a5c5473c
w91379137 <w913791379137@gmail.com>
2019-10-09 17:53:52 +0800

    增加 寫入 log 功能

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,21 @@ let debug_AC = require('debug')('app:a:c')
 let debug_BC = require('debug')('app:b:c')
 let debug_AB = require('debug')('app:a:b')
 
+const fs = require('fs-js');
+
 setTimeout(() => {
     debug_AC('Build env_ac:' + env);
     debug_BC('Build env_bc:' + env);
     debug_AB('Build env_ab:' + env);
 }, 100);
+
+function write(data) {
+    fs.appendFile('log/data.txt', data + '\n', (err) => {
+        if (err) throw err;
+    });
+}
+
+write('test');
 
 // class Foo {
 //     constructor() { this.bar(); }
