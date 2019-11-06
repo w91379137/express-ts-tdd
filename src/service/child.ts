8e5dd7643ee05615e3c637b2ef6047a4cb4b75b8
w91379137 <w913791379137@gmail.com>
2019-11-06 17:59:04 +0800

    s

--- src/service/child.ts
+++ src/service/child.ts
@@ -11,6 +11,7 @@ export var terminal = 1;
 
 // 四個方法的解釋
 // https://dzone.com/articles/understanding-execfile-spawn-exec-and-fork-in-node
+// https://carlos-studio.com/2017/09/04/node-js-%E4%BD%BF%E7%94%A8-child_process-%E6%A8%A1%E7%B5%84%E5%BB%BA%E7%AB%8B%E5%AD%90%E8%99%95%E7%90%86%E7%A8%8B%E5%BA%8F/
 
 // ipc 通訊
 // https://www.byvoid.com/zht/blog/node-child-process-ipc
