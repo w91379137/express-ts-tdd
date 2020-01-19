cda3e0b442ec793a4121ef8419e0b8291e837218
w91379137 <w913791379137@gmail.com>
2020-01-19 17:11:40 +0800

    改寫 api 寫法

--- src/server/controller/root-controller.ts
+++ src/server/controller/root-controller.ts
@@ -10,26 +10,29 @@ export class RootController implements Controller {
 
     constructor() {
 
-        this.router.get('/', async (req, res) => {
-            debug('get')
-            GlobalUse.myStorage.setItem('time', (new Date()).toString());
-            debug(GlobalUse.myStorage.getItem('time'));
-            res.status(200).send({
-                success: true,
-                result: 'result',
-            })
+        this.router.get('/', this.getTest)
+        this.router.post('/', this.postTest)
+
+    }
+
+    getTest = async (req, res) => {
+        debug('get')
+        GlobalUse.myStorage.setItem('time', (new Date()).toString());
+        debug(GlobalUse.myStorage.getItem('time'));
+        res.status(200).send({
+            success: true,
+            result: 'result',
         })
+    }
 
-        this.router.post('/', async (req, res) => {
+    postTest = async (req, res) => {
 
-            let body = req.body
-            debug('post', body)
-            res.status(200).send({
-                success: true,
-                result: body,
-            })
+        let body = req.body
+        debug('post', body)
+        res.status(200).send({
+            success: true,
+            result: body,
         })
-
     }
 
 }
