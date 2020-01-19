afdc4306b94f6c0cf302710b419030da7a0494e1
w91379137 <w913791379137@gmail.com>
2020-01-19 17:07:35 +0800

    調整 app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,13 @@ require('source-map-support').install();
 let debug = require('debug')('app:main')
 
 //====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
-// 本地服務建立
+// local 服務建立
 
 import { GlobalUse } from './global-use';
-
 import { storage } from './service/localstorage';
-GlobalUse.myStorage = storage
-// GlobalUse.myStorage.setItem('time', (new Date()).toString());
-// GlobalUse.myStorage.getItem('time'));
-
 import { append } from './service/append';
-GlobalUse.append = append
-// GlobalUse.append("data/data.txt", "test")
-
 import { log } from './service/log';
-GlobalUse.log = log
-// GlobalUse.log("test")
-
 import { terminal } from './service/child';
-debug(terminal)
 
 //====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
 // server 屬性建立
@@ -31,21 +19,43 @@ import * as express from 'express';
 import { Server } from './server/server';
 import { RootController } from './server/controller/root-controller';
 
-// Middleware 中介軟體
-function loggerMiddleware(req: express.Request, res: express.Response, next) {
-    debug(`Method:${req.method} Path:${req.path} Body:${JSON.stringify(req.body)}`);
-    next();
+async function run() {
+
+    //====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+    // local 服務建立
+    GlobalUse.myStorage = storage
+    // GlobalUse.myStorage.setItem('time', (new Date()).toString());
+    // GlobalUse.myStorage.getItem('time'));
+
+    GlobalUse.append = append
+    // GlobalUse.append("data/data.txt", "test")
+
+    GlobalUse.log = log
+    // GlobalUse.log("test")
+
+    debug(terminal)
+
+    //====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+    // server 屬性建立
+
+    // Middleware 中介軟體
+    function loggerMiddleware(req: express.Request, res: express.Response, next) {
+        debug(`Method:${req.method} Path:${req.path} Body:${JSON.stringify(req.body)}`)
+        next();
+    }
+
+    const port = 5000
+    let server = new Server({
+        port: port,
+        controllers: [
+            new RootController(),
+        ],
+        middlewares: [
+            loggerMiddleware
+        ],
+    })
+
+    server.start()
 }
 
-const port = 5000;
-let server = new Server({
-    port: port,
-    controllers: [
-        new RootController()
-    ],
-    middlewares: [
-        loggerMiddleware
-    ],
-})
-
-server.start()
+run()
