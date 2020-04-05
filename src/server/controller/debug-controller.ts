20d1a41fca3467a69a9470da14d64f0fe8ece217
w91379137 <w913791379137@gmail.com>
2020-04-05 23:49:31 +0800

    新增 查找 規則

--- src/server/controller/debug-controller.ts
+++ src/server/controller/debug-controller.ts
@@ -1,22 +1,56 @@
 const express = require('express');
-let debug = require('debug')('app:root-controller');
+let debug = require('debug')('app:debug-controller');
 import { Controller } from "./controller";
 import { GlobalUse } from "../../global-use";
 
+/*
+http://localhost:5000/debug/?path=example
+http://localhost:5000/debug/?path=example_obj
+http://localhost:5000/debug/?path=example_arr
+http://localhost:5000/debug/?path=example_arr_1
+ */
 export class DebugController implements Controller {
 
     path = '/debug'
     router = express.Router()
 
     constructor() {
-
         this.router.get('/', this.get)
-
+        GlobalUse['example'] = debugExample()
     }
 
     get = async (req, res) => {
-        res.status(200).send(htmlRelaod({ a: 1 }))
+
+        let path = (req.query.path || '') as string
+        let keyArr = pathParse(path)
+
+        let obj = GlobalUse as any
+        let check = []
+        for (const key of keyArr) {
+
+            check.push(key)
+            obj = obj[key]
+
+            if (!obj) {
+                obj = {
+                    path,
+                    msg: `no value at ${check.join('_')}`
+                }
+                break
+            }
+        }
+
+        res.status(200).send(htmlRelaod(obj))
     }
+}
+
+export function pathParse(path: string): any[] {
+    return path.split('_')
+        .filter(str => !!str) // 除去空字串
+        .filter(str => {
+            let maybeInt = parseInt(str)
+            return isNaN(maybeInt) ? str : maybeInt
+        }) // 把文字轉數字
 }
 
 function htmlRelaod(obj: any, time: number = 0) {
@@ -47,4 +81,20 @@ function htmlRelaod(obj: any, time: number = 0) {
         <body>
         <plaintext>${content}
         `
+}
+
+function debugExample() {
+    return {
+        obj: {
+            hello: 'debug'
+        },
+        arr: [
+            {
+                hello: 'debug'
+            },
+            {
+                hello2: 'debug2'
+            },
+        ]
+    }
 }
